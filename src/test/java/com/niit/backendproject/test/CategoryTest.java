cee9ac1b6125cc7dd7908e7212c9791eca4ab83e
Jayanth-vasu <jayanthvasu96@gmail.com>
2016-07-22 22:44:31 +0530

    mapping with database

---
+++ src/test/java/com/niit/backendproject/test/CategoryTest.java
@@ -0,0 +1,25 @@
+package com.niit.backendproject.test;
+
+
+	import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+	import com.niit.backendproject.dao.CategoryDAO;
+	import com.niit.backendproject.model.Category;
+
+
+	public class CategoryTest {
+		public static void main(String[] args) {
+			AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+			context.scan("com.niit.backendproject");
+			context.refresh();
+			
+			CategoryDAO categoryDAO = (CategoryDAO) context.getBean("categoryDAO");
+			Category category= (Category) context.getBean("category");
+			category.setCategory_id("CG189");
+			category.setDescription("Television");
+			category.setCategory_name("LG");
+			categoryDAO.saveOrUpdate(category);
+		context.close();
+		}
+		}
+
